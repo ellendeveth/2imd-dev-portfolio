497d12bd0dec4ea5ea8f80e4189f2a7d3aec3140
Ellen <ellen.deveth@hotmail.com>
2022-03-03 13:56:11 +0100

    fix: delete error messages off script

--- Lab3-ES6/main.js
+++ Lab3-ES6/main.js
@@ -1,9 +1,4 @@
 import './style.css'
 import App from "./classes/App";
 
-<<<<<<< HEAD
-=======
-import App from "./classes/App";
-
->>>>>>> b588844d0a684063487462efd699db95eb4c0757
 const app = new App();
