33f877ad27f0f91d8309395eeb0dbb118b111576
Ellen <ellen.deveth@hotmail.com>
2022-03-03 13:05:17 +0100

    fix: fix error

--- Lab3-ES6/classes/App.js
+++ Lab3-ES6/classes/App.js
@@ -1,4 +1,4 @@
-import Todo from `./Todo`
+import Todo from "./Todo"
 
 export default class App {
     constructor() {
--- Lab3-ES6/main.js
+++ Lab3-ES6/main.js
@@ -1,6 +1,4 @@
 import './style.css'
+import App from "./classes/App";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const app = new App();
